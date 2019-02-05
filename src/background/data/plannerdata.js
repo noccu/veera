noccu/veera

commit 966e2a0ef7480dbdec2fd7057f78b998c70ea2b0

Upd: Maintain capitalization in planner selections. Upd: Rename Revenant weapons to show characters in the planner. Fix: Template mappings (didn't actually affect anything so far... probably) Fix: Hide templates in the planner dropdowns.

src/background/data/plannerdata.js
@@ -5,100 +5,100 @@ const PLANNER_TEMPLATES = {
     orbs: {
         low: {
             type: SUPPLYTYPE.treasure,
-            fire: 1011,
-            water: 1021,
-            earth: 1031,
-            wind: 1041,
-            light: 1051,
-            dark: 1061
+            Fire: 1011,
+            Water: 1021,
+            Earth: 1031,
+            Wind: 1041,
+            Light: 1051,
+            Dark: 1061
         },
         high: {
             type: SUPPLYTYPE.treasure,
-            fire: 1012, 
-            water: 1022, 
-            earth: 1032,
-            wind: 1042,
-            light: 1052,
-            dark: 1062,
+            Fire: 1012, 
+            Water: 1022, 
+            Earth: 1032,
+            Wind: 1042,
+            Light: 1052,
+            Dark: 1062,
         }
     },
     whorls: {
         type: SUPPLYTYPE.treasure,
-        fire: 1313, 
-        water: 1323, 
-        earth: 1333,
-        wind: 1343,
-        light: 1353,
-        dark: 1363
+        Fire: 1313, 
+        Water: 1323, 
+        Earth: 1333,
+        Wind: 1343,
+        Light: 1353,
+        Dark: 1363
     },
     tomes: {
         type: SUPPLYTYPE.treasure,
-        fire: 1311, 
-        water: 1321, 
-        earth: 1331,
-        wind: 1341,
-        light: 1351,
-        dark: 1361
+        Fire: 1311, 
+        Water: 1321, 
+        Earth: 1331,
+        Wind: 1341,
+        Light: 1351,
+        Dark: 1361
     },
     scrolls: {
         type: SUPPLYTYPE.treasure,
-        fire: 1312, 
-        water: 1322, 
-        earth: 1332,
-        wind: 1342,
-        light: 1352,
-        dark: 1362
+        Fire: 1312, 
+        Water: 1322, 
+        Earth: 1332,
+        Wind: 1342,
+        Light: 1352,
+        Dark: 1362
     },
     scales: {
         type: SUPPLYTYPE.treasure,
-        fire: 1111, 
-        water: 1121, 
-        earth: 1131,
-        wind: 1141,
-        light: 1151,
-        dark: 1161
+        Fire: 1111, 
+        Water: 1121, 
+        Earth: 1131,
+        Wind: 1141,
+        Light: 1151,
+        Dark: 1161
     },
     stones: {
         type: SUPPLYTYPE.treasure,
-        sword: 4011,
-        dagger: 4021,
-        spear: 4031,
-        axe: 4041,
-        staff: 4051,
-        gun: 4061,
-        fist: 4071,
-        bow: 4081,
-        harp: 4091,
-        katana: 4101
+        Sword: 4011,
+        Dagger: 4021,
+        Spear: 4031,
+        Axe: 4041,
+        Staff: 4051,
+        Gun: 4061,
+        Fist: 4071,
+        Bow: 4081,
+        Harp: 4091,
+        Katana: 4101
     },
     trueAnima: {
         type: SUPPLYTYPE.treasure,
-        fire: 41, 
-        water: 42, 
-        earth: 43,
-        wind: 44,
-        light: 45,
-        dark: 46
+        Fire: 41, 
+        Water: 42, 
+        Earth: 43,
+        Wind: 44,
+        Light: 45,
+        Dark: 46
 },
     anima: {
         primal: {
             type: SUPPLYTYPE.treasure,
-            fire: 11, 
-            water: 12, 
-            earth: 13,
-            wind: 10,
-            light: 25,
-            dark: 30
+            Fire: 11, 
+            Water: 12, 
+            Earth: 13,
+            Wind: 10,
+            Light: 25,
+            Dark: 30
         }
     },
     omegaItem: {
         type: SUPPLYTYPE.treasure,
-        fire: 47, 
-        water: 48, 
-        earth: 49,
-        wind: 32,
-        light: 50,
-        dark: 51
+        Fire: 47, 
+        Water: 48, 
+        Earth: 49,
+        Wind: 32,
+        Light: 50,
+        Dark: 51
     }
 };
 
@@ -544,24 +544,24 @@ window.PlannerData = {
             //crystal 500
         ],
         wtype: {
-            sword: null,
-            axe: null,
-            spear: null,
-            staff: null,
-            gun: null,
-            fist: null,
-            katana: null,
-            harp: null,
-            dagger: null,
-            bow: null,
+            "Uno (spear)": null,
+            "Sorn (bow)": null,
+            "Sarasa (axe)": null,
+            "Feower (dagger)": null,
+            "Funf (staff)": null,
+            "Six (fist)": null,
+            "Siete (sword)": null,
+            "Okto (katana)": null,
+            "Nio (harp)": null,
+            "Esser (gun)": null,
         },
         element: {
-            fire: null, 
-            water: null, 
-            earth: null,
-            wind: null,
-            light: null,
-            dark: null,
+            Fire: null, 
+            Water: null, 
+            Earth: null,
+            Wind: null,
+            Light: null,
+            Dark: null,
             templates: [
                 supplyTemplate(2, PLANNER_TEMPLATES.orbs.low, 250),
                 supplyTemplate(2, PLANNER_TEMPLATES.whorls, 250),
@@ -595,6 +595,29 @@ window.PlannerData = {
                 supplyTemplate(7, PLANNER_TEMPLATES.omegaItem, 60),
             ]            
         },
-        stepNames: ["Revenant", "Awaken", "Elechange", "Upgrade 1", "Upgrade 2", "Upgrade 3", "Upgrade 4", "Upgrade 5", "Upgrade 6"]
+        stepNames: ["Revenant", "Awaken", "Elechange", "Upgrade 1", "Upgrade 2", "Upgrade 3", "Upgrade 4", "Upgrade 5", "Upgrade 6"],
+        typeNames: {
+            "Uno (spear)": "Spear",
+            "Sorn (bow)": "Bow",
+            "Sarasa (axe)": "Axe",
+            "Feower (dagger)": "Dagger",
+            "Funf (staff)": "Staff",
+            "Six (fist)": "Fist",
+            "Siete (sword)": "Sword",
+            "Okto (katana)": "Katana",
+            "Nio (harp)": "Harp",
+            "Esser (gun)": "Gun",
+        }
     }
 };
+
+(function hideTemplates(o) {
+    if (o["templates"]) {
+        Object.defineProperty(o, "templates", {enumerable: false});
+    }
+    else {
+        for (let k of Object.keys(o)) {
+            if (o[k] instanceof Object) { hideTemplates(o[k]); }
+        }
+    }
+})(PlannerData);

src/background/planner.js
@@ -21,13 +21,14 @@ window.Planner = {
                  case "wtype":
                      t = PlannerData[series].wtype.templates || [];
                      itemArray = t.concat(PlannerData[series].wtype[wtype]);
-                     templateKey = wtype;
+                     templateKey = PlannerData[series].typeNames ? PlannerData[series].typeNames[wtype] : wtype;
                      break;
                  case "element":
                      t = PlannerData[series].element.templates || [];
                      itemArray = t.concat(PlannerData[series].element[element]);
                      templateKey = element;
                      break;
+                 case "typeNames":
                  case "stepNames":
                      continue;
                  default:

commit e8f08e204fe6f6d162bc4c54e871b2cdc621d315

Fix a warning regarding item data. Add farm locations to commonly used treasure tooltips. (partial data, possibly temporary, requires visiting supplies in-game once and then reloading Veera) Moving towards the wiki milestone, could be seen as part of #1. Also of note for #4. Fix planner not showing some items (broke in 966e2a0ef7480dbdec2fd7057f78b998c70ea2b0, oops!)

src/background/data/plannerdata.js
@@ -478,9 +478,9 @@ window.PlannerData = {
     
     Revenant:{
         core: [
-            createItem(1, SUPPLYTYPE.treasure, PLANNER_TEMPLATES.orbs.high.light, 50),
-            createItem(1, SUPPLYTYPE.treasure, PLANNER_TEMPLATES.scrolls.light, 50),
-            createItem(1, SUPPLYTYPE.treasure, PLANNER_TEMPLATES.whorls.light, 50),
+            createItem(1, SUPPLYTYPE.treasure, PLANNER_TEMPLATES.orbs.high.Light, 50),
+            createItem(1, SUPPLYTYPE.treasure, PLANNER_TEMPLATES.scrolls.Light, 50),
+            createItem(1, SUPPLYTYPE.treasure, PLANNER_TEMPLATES.whorls.Light, 50),
             createItem(1, SUPPLYTYPE.treasure, 1151, 50),
             createItem(1, SUPPLYTYPE.treasure, 2001, 50),
 //            createItem(1, SUPPLYTYPE.treasure, 1052, 50),
@@ -520,24 +520,24 @@ window.PlannerData = {
             //crystal 400
             
             //5
-            createItem(7, SUPPLYTYPE.treasure, PLANNER_TEMPLATES.omegaItem.wind, 20),
-            createItem(7, SUPPLYTYPE.treasure, PLANNER_TEMPLATES.omegaItem.fire, 20),
-            createItem(7, SUPPLYTYPE.treasure, PLANNER_TEMPLATES.omegaItem.water, 20),
-            createItem(7, SUPPLYTYPE.treasure, PLANNER_TEMPLATES.omegaItem.earth, 20),
-            createItem(7, SUPPLYTYPE.treasure, PLANNER_TEMPLATES.omegaItem.light, 20),
-            createItem(7, SUPPLYTYPE.treasure, PLANNER_TEMPLATES.omegaItem.dark, 20),
+            createItem(7, SUPPLYTYPE.treasure, PLANNER_TEMPLATES.omegaItem.Wind, 20),
+            createItem(7, SUPPLYTYPE.treasure, PLANNER_TEMPLATES.omegaItem.Fire, 20),
+            createItem(7, SUPPLYTYPE.treasure, PLANNER_TEMPLATES.omegaItem.Water, 20),
+            createItem(7, SUPPLYTYPE.treasure, PLANNER_TEMPLATES.omegaItem.Earth, 20),
+            createItem(7, SUPPLYTYPE.treasure, PLANNER_TEMPLATES.omegaItem.Light, 20),
+            createItem(7, SUPPLYTYPE.treasure, PLANNER_TEMPLATES.omegaItem.Dark, 20),
             createItem(7, SUPPLYTYPE.treasure, PLANNER_ITEMS.antiqueCloth, 100),
             createItem(7, SUPPLYTYPE.treasure, PLANNER_ITEMS.supremeMerit, 10),
             createItem(7, SUPPLYTYPE.treasure, PLANNER_ITEMS.blueSkyCrystal, 15),
             //crystals 500
             
             //6
-            createItem(8, SUPPLYTYPE.treasure, PLANNER_TEMPLATES.trueAnima.fire, 3),
-            createItem(8, SUPPLYTYPE.treasure, PLANNER_TEMPLATES.trueAnima.water, 3),
-            createItem(8, SUPPLYTYPE.treasure, PLANNER_TEMPLATES.trueAnima.earth, 3),
-            createItem(8, SUPPLYTYPE.treasure, PLANNER_TEMPLATES.trueAnima.wind, 3),
-            createItem(8, SUPPLYTYPE.treasure, PLANNER_TEMPLATES.trueAnima.light, 3),
-            createItem(8, SUPPLYTYPE.treasure, PLANNER_TEMPLATES.trueAnima.dark, 3),
+            createItem(8, SUPPLYTYPE.treasure, PLANNER_TEMPLATES.trueAnima.Wire, 3),
+            createItem(8, SUPPLYTYPE.treasure, PLANNER_TEMPLATES.trueAnima.Fater, 3),
+            createItem(8, SUPPLYTYPE.treasure, PLANNER_TEMPLATES.trueAnima.Warth, 3),
+            createItem(8, SUPPLYTYPE.treasure, PLANNER_TEMPLATES.trueAnima.Eind, 3),
+            createItem(8, SUPPLYTYPE.treasure, PLANNER_TEMPLATES.trueAnima.Light, 3),
+            createItem(8, SUPPLYTYPE.treasure, PLANNER_TEMPLATES.trueAnima.Dark, 3),
             createItem(8, SUPPLYTYPE.treasure, PLANNER_ITEMS.rainbowPrism, 250),
             createItem(8, SUPPLYTYPE.treasure, PLANNER_ITEMS.blueSkyCrystal, 30)
             //Gold brick 1

src/background/planner.js
@@ -53,7 +53,8 @@ window.Planner = {
                                         id: id,
                                         name: supplydata.name,
                                         needed: item.needed,
-                                        current: supplydata.count
+                                        current: supplydata.count,
+                                        loc: supplydata.questLocation
                                     });
                                 }
                             }

src/background/supplies.js
@@ -172,6 +172,20 @@ const ITEM_KIND = {
         "path": "item/recycling"
     }
 };
+const TREASURE_SOURCES = { //list of item id -> quest id, quest name for farming. TODO: add IDs also move these 2 consts to own file.
+    2: {id: -1, name: "Scattered Cargo (1/115)"},
+    3: {id: -1, name: "Scattered Cargo (1/115)"},
+    5: {id: -1, name: "Special Op's Request (8) or Lucky Charm Hunt (6)"},
+    6: {id: -1, name: "Threat to the Fisheries (9)"},
+    8: {id: -1, name: "Whiff of Danger (13/39/52) or The Fruit of Lumacie (13/39/52)"},
+    17: {id: -1, name: "Whiff of Danger (13/39/52) or The Fruit of Lumacie (13/39/52)"},
+    22: {id: -1, name: "What's in the Box? (20) or I Challenge You! (17, use Belle Sylphid)"},
+    24: {id: -1, name: "Strength and Chivalry (18) or What's in the Box? (20)"},
+    28: {id: -1, name: "Playing Cat and Mouse (22) or For Whom the Bell Tolls (22)"},
+    29: {id: -1, name: "Playing Cat and Mouse (22) or For Whom the Bell Tolls (22)"},
+    39: {id: -1, name: "Miscongeniality (32/41) or New Leaf (30/44/65) or The Dungeon Diet (30/44/65)"},
+    40: {id: -1, name: "Miscongeniality (32/41) or New Leaf (30/44/65) or The Dungeon Diet (30/44/65)"}
+};
 
 window.Supplies = {
     treasure: {
@@ -186,6 +200,7 @@ window.Supplies = {
                     category: item.category_type, 
                     count: parseInt(item.number)
                     };
+                checkFarmLocation(item.item_id);
             }
             
             Supplies.save("t");
@@ -330,10 +345,16 @@ window.Supplies = {
     }
 };
 
+function checkFarmLocation (id) {
+    if (id in TREASURE_SOURCES) {
+        Supplies.treasure.index[id].questLocation = TREASURE_SOURCES[id].name;
+    }
+}
+
 function gotQuestLoot(data) {
     var upd = [];
     function makeUpd(item) {
-        let itemData = ITEM_KIND[item.item_kind];
+        let itemData = ITEM_KIND[item.item_kind || item.kind];
         if (!itemData || itemData.manual) {
             console.warn("Uncertain item type, errors may occur.", JSON.parse(JSON.stringify(item)));
             if (!itemData) { itemData = {path: item.type && item.type.includes("item") ? item.type : ITEM_KIND[10].path}; } //default to treasure/article, seems most common. also path's the only thing used so far...

src/pages/devtools/main.js
@@ -130,7 +130,8 @@ function updateTreasure (idx) {
             var li = createSupplyItem("t-" + id,
                                       idx[id].name, 
                                       idx[id].count, 
-                                      createSupplyURL(id, "article"));
+                                      createSupplyURL(id, "article"),
+                                      idx[id].questLocation);
             temp.content.appendChild(li);
         }
     }
@@ -155,14 +156,14 @@ function updateConsumables (data) {
     list.appendChild(temp.content);
 }
 
-function createSupplyItem (id, name, num, thumb) {
+function createSupplyItem (id, name, num, thumb, loc) {
     var t = document.getElementById("template-supply-item");
 /*    t.content.querySelector("li").title = name;
     t.content.querySelector("img").src = thumb;
     t.content.querySelector("div").textContent = num;*/
     let item = t.content.firstElementChild;
     item.id = id;
-    item.title = name;
+    item.title = loc ? `${name}\nGet from: ${loc}`: name;
     item.getElementsByClassName("collection-icon")[0].src = thumb;
     item.getElementsByClassName("collection-data")[0].textContent = num;
     
@@ -178,12 +179,10 @@ function createSupplyURLkind (id, path) {
 }
 
 //Planner functions
-function createPlannerItem (name, current, needed, thumb) {
-        var t = document.getElementById("template-supply-item");
-        t.content.querySelector("li").title = name;
-        t.content.querySelector("img").src = thumb;
-        t.content.querySelector("div").innerHTML = `<span class="planner-current">${current}</span> /<span class="planner-needed">${needed}</span>`;
-        return document.importNode(t.content, true);
+function createPlannerItem (id, name, current, needed, thumb, loc) {
+        let item = createSupplyItem("p"+id, name, 0, thumb, loc);
+        item.querySelector(".collection-data").innerHTML = `<span class="planner-current">${current}</span> /<span class="planner-needed">${needed}</span>`;
+        return item;
 }
 
 function changeSeries(ev) { //Event handler, updates type and element list when series changes

src/pages/devtools/ui.js
@@ -171,7 +171,7 @@ window.UI = {
             for (let item of plan) {
 //                console.log(item);
                 
-                entry = createPlannerItem(item.name, item.current, item.needed, createSupplyURL(item.id, item.type));
+                entry = createPlannerItem(item.id, item.name, item.current, item.needed, createSupplyURL(item.id, item.type), item.loc);
                 entry.firstElementChild.dataset.id = item.id;
                 entry.firstElementChild.dataset.needed = item.needed;
                 entry.firstElementChild.dataset.type = item.type;