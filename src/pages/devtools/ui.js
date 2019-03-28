noccu/veera

commit 46c63c594e88409ba8ed81e2794b2460ab0aec55

Change filter behaviour for raids.
Some filters make more sense to AND with others, so do exactly that.
This effectively splits filters into "special" (AND) and "normal" (OR).
Not really liking how it does, but it does!
Also adds a filter (inactive).

src/pages/devtools/ui.js
@@ -214,7 +214,7 @@ function evhGlobalClick (e) {
                 break;
             case "filter":
                 let activeFilters = [];
-                if (e.target.dataset.value == "All") {
+                if (e.target.dataset.value == "ALL") {
                     for (let filter of e.target.parentElement.children) {
                         filter.classList.remove("active");
                     }
@@ -273,14 +273,51 @@ function evhSuppliesFilter (e) {
     }
 }
 function evhRaidsFilter (e) {
-//    var list = document.getElementById("supplies-list");
-//    var r = new RegExp(e.target.dataset.value, "i"); //Faster
-    let filters = e.detail,
-        showAll = filters[0] == "All",
-        filterables = ["isHl", "elementName"];
+    let filters = e.detail;
+    let filterables = ["elementName"];
+    
+    let check = {hl: {do: false},
+                 inactive: {do: false},
+                 active: {do: true}};
+    function filter (raid) {
+        let specialCasesFound = 0; //Allow Special cases to AND each other when they are the only filters.
+        check.active.val = raid.entryObj.active == true;
+        
+        let ret = filters.some(f => { //This requires special case filters to be before general ones. If unwanted then update to do a full loop.
+            switch (f) {
+                case "ALL": //Actually more like ACTIVE
+                    return true;
+                case "INACTIVE":
+                    //Delay actual checking so we can AND with other filters if needed.
+                    check.inactive.do = true;
+                    check.active.do = false;
+                    check.inactive.val = !check.active.val;
+                    specialCasesFound++;
+                    if (filters.length == specialCasesFound) {return true;} //Allow to check other filters if they exist, return if not.
+                    break;
+                case "HL":
+                    check.hl.do = true;
+                    check.hl.val = raid.entryObj.data.isHl;
+                    specialCasesFound++;
+                    if (filters.length == specialCasesFound) {return true;}
+                    break;
+                default:
+                    return filterables.some(k => raid.entryObj.data[k] == f);
+            }
+        });       
+        if (ret) { //AND special cases
+            for (let c in check) {
+                if (check[c].do) {
+                    ret = ret && check[c].val;
+                }
+            }
+        }
+
+        return ret;
+    }
     
     for (let raid of UI.raids.list) {
-            if (showAll || filters.some(f => filterables.some(k => raid.entryObj.data[k] == f))) {
+            if (filter(raid)) {
                 raid.classList.remove("hidden");
             }
             else {

src/background/data/raiddata.js
@@ -27,7 +27,7 @@ function RaidData(name, id, diff, minHostRank, dailyHosts, apCost, matIDs, matNu
     this.id = id;
     this.diff = diff;
     this.minHostRank = minHostRank;
-    this.isHl = minHostRank > 100 ? "hl" : false;
+    this.isHl = minHostRank > 100;
     this.dailyHosts = dailyHosts;
     this.apCost = apCost;
     this.matCost = mats;

src/pages/devtools/ui.html
@@ -132,7 +132,7 @@
                         <div class="panel-content">
                             <div class="panel-filter">
                                 <ul id="supplies-filter">
-                                    <li class="active" data-event="filter" data-value="All">All</li>
+                                    <li class="active" data-event="filter" data-value="ALL">All</li>
 <!--                                    Todo: fill list programatically-->
                                     <li data-event="filter" data-value="Consumables" data-meta="true">Consumabes</li>
                                     <li data-event="filter" data-value="Treasure">Treasure</li>
@@ -181,7 +181,7 @@
                     <div class="panel-content">
                         <div class="panel-filter">
                             <ul id="raids-current-filter">
-                                <li class="active" data-event="filter" data-value="all">All</li>
+                                <li class="active" data-event="filter" data-value="ALL">All</li>
                                 <li data-event="filter" data-value="ssr">SSR only</li>
                             </ul>
                         </div>
@@ -224,8 +224,10 @@
                     <div class="panel-content">
                         <div class="panel-filter">
                             <ul id="raids-filter">
-                                <li class="active" data-event="filter" data-value="All">All</li>
-                                <li data-event="filter" data-value="hl">HL</li>
+                                <li class="active" data-event="filter" data-value="ALL">All</li>
+                                <li data-event="filter" data-value="INACTIVE">Inactive</li>
+<!--                                Values here should be synced to raiddata.js-->
+                                <li data-event="filter" data-value="HL">HL</li>
                                 <li data-event="filter" data-value="fire">Fire</li>
                                 <li data-event="filter" data-value="water">Water</li>
                                 <li data-event="filter" data-value="earth">Earth</li>

commit c2e4d06f820899e37742abd98928b3502188bba5

Fix supplies filter breaking on display all.
Regression from 46c63c594e88409ba8ed81e2794b2460ab0aec55
Also rewrote it to match the raids filter.
Kind of weird now but easier to update in the future.

src/pages/devtools/ui.js
@@ -295,11 +295,19 @@ function evhSuppliesSearch (e) { //just treasure for now
 function evhSuppliesFilter (e) {
     var list = document.getElementById("supplies-list");
 //    var r = new RegExp(e.target.dataset.value, "i"); //Faster
-    let filters = e.detail,
-        showAll = filters[0] == "All";
-    
+    let filters = e.detail;
+    function filter (item){
+        return filters.some(f => {
+            switch (f) {
+                case "ALL":
+                case item.dataset.type:
+                case item.dataset.metaType:
+                    return true;
+            }
+        });
+    }
     for (let item of list.children) {
-        if (showAll || filters.some(f => f == item.dataset.type || (item.dataset.metaType && f == item.dataset.metaType))) {
+        if (filter(item)) {
             item.classList.remove("hidden");
         }
         else {