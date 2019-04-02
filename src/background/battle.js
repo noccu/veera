noccu/veera

commit 611558aab18c0ccf5a61f1c4a0cd14abae4398b3

Display additional battle stats #2.
- Add skill dmg, echo dmg and heal amounts.
- Add skill names to "in-turn" skills (counters, etc).
- Change character battle stats to CSS grid layout.
  - Kind of nicer to work with.
  - Seems to fix size inconsistencies when rescaling.

src/background/battle.js
@@ -197,6 +197,7 @@ function BattleActionData(action) {
     this.hits = 0;
     this.echoDmg = 0;
     this.echoHits = 0;
+    this.skillDmg = 0;
     this.crits = 0;
     this.critDmg = 0;
     this.misses = 0;
@@ -299,6 +300,7 @@ function BattleCharData(id, name = "") {
         hits: 0,
         echoDmg: 0,
         echoHits: 0,
+        skillDmg: 0,
         crits: 0,
         da: 0,
         ta: 0,
@@ -331,7 +333,13 @@ const battleCharStatsShared = {
         return safeDivide(this.ougis, this.instance.activeTurns) * 100;
     },
     get totalDmg() {
-        return this.dmg + this.echoDmg;
+        return this.dmg + this.echoDmg + this.skillDmg;
+    },
+    get skillDmgPerc() {
+        return safeDivide(this.skillDmg, this.totalDmg) * 100;
+    },
+    get echoDmgPerc() {
+        return safeDivide(this.echoDmg, this.totalDmg) * 100;
     },
     get totalHits() {
         return this.hits + this.echoHits;
@@ -373,7 +381,12 @@ function battleParseDamage(input, actionData, type) {
                         actionData.echoHits++;
                     }
                     else {
-                        actionData.dmg += parseInt(entry.value);
+                        if (actionData.action == BATTLE_ACTIONS.skill) {
+                            actionData.skillDmg += parseInt(entry.value);
+                        }
+                        else {
+                            actionData.dmg += parseInt(entry.value);
+                        }
                         actionData.hits++;
                     }
 
@@ -536,6 +549,7 @@ function battleAttack(json) {
                 //Also some bosses cast them from buffs (e.g. Alex plain dmg buff)
                 if (action.to == "player") {
                     actionData = new BattleActionData(BATTLE_ACTIONS.skill);
+                    actionData.name = action.name;
                     actionData.char = Battle.current.characters.getAtPos(action.pos).char;
                 }
                 else {//boss
@@ -646,6 +660,7 @@ function updateBattleStats(actionData) {
         charStats.dmg += actionData.dmg;
         charStats.echoHits += actionData.echoHits;
         charStats.echoDmg += actionData.echoDmg;
+        charStats.skillDmg += actionData.skillDmg;
         charStats.crits += actionData.crits;
         if (actionData.action == BATTLE_ACTIONS.ougi) {
             charStats.ougis++;

src/pages/devtools/ui.html
@@ -286,6 +286,18 @@
                                     </div>
                                     <div class="battle-stats" data-stat="critRate">Crit: <span data-deci="true">0</span>%</div>
                                     <div class="battle-stats" data-stat="ougiRate">Ougi: <span data-deci="true">0</span>%</div>
+                                    <div class="sep"></div>
+                                    <div class="battle-stats-group chara">
+                                        <div class="battle-stats" data-stat="skillDmg">Skill dmg: <span>0</span></div>
+                                        <div class="battle-stats" data-stat="echoDmg">Echo dmg: <span>0</span></div>
+                                    </div>
+                                    <div class="battle-stats-group chara">
+                                        <div class="battle-stats" data-stat="skillDmgPerc">Skill %: <span>0</span></div>
+                                        <div class="battle-stats" data-stat="echoDmgPerc">Echo %: <span>0</span></div>
+                                    </div>
+                                    <div class="sep"></div>
+                                    <div class="battle-stats" data-stat="selfHealing">Heal (self): <span>0</span></div>
+                                    <div class="battle-stats" data-stat="teamHealing">Heal (team): <span>0</span></div>
                                 </div>
                             </div>
                         </template>

src/pages/stylesheets/ui.css
@@ -190,9 +190,6 @@ li {
 .spacer {
     margin-bottom: 1em;
 }
-
-
-
 .sep {
     margin-bottom: 0.5em;
     padding-bottom: 0.5em;
@@ -373,32 +370,45 @@ button {
     padding: 0.3em;
 }
 
+/* Battle */
 .battle-char-container {
     width: 32%;
     margin: 0.5em 0.2em;
     display: inline-block;
 }
-
+.battle-stats-container { /*Used only for chars it seems*/
+    text-align: left;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-template-areas: "left right";
+}
+.battle-stats-container .sep {
+    grid-area: auto / left / auto / right;
+}
 .battle-stats {
     display: inline-block;
+/*
     margin-right: 2em;
     text-align: left;
+*/
 }
-
-.battle-stats-group .battle-stats {
-    display: block;
-    margin-right: 0;
-}
-
 .battle-stats-group {
     display: inline-block;
+/*
     margin-right: 2em;
     max-width: 20%
+*/
+}
+.battle-stats-group .battle-stats {
+    display: block;
+/*    margin-right: 0;*/
 }
 
 .battle-stats-group.chara {
+/*
     max-width: 49%;
     margin-right: 1em
+*/
 }
 
 .battle-char-name {
@@ -410,9 +420,6 @@ button {
     text-align: center;
 }
 
-.battle-stats-container {
-    text-align: left;
-}
 
 #raid-current-drop-list {
     padding: 0.5em;

commit 2e938bb4e657da69bbdd7c6146320391c2cf8dd7

Battle updates/fixes.
- Add ougi dmg to battle stats.
- Fix skill dmg not calculated in total dmg for actions.
  - Probably caused inaccurate avg dmg/turn stats.
  - Regression from 611558aab18c0ccf5a61f1c4a0cd14abae4398b3, oops!
- Allow collapsing of extra stats.

src/background/battle.js
@@ -198,6 +198,7 @@ function BattleActionData(action) {
     this.echoDmg = 0;
     this.echoHits = 0;
     this.skillDmg = 0;
+    this.ougiDmg = 0;
     this.crits = 0;
     this.critDmg = 0;
     this.misses = 0;
@@ -209,7 +210,7 @@ function BattleActionData(action) {
 Object.defineProperties(BattleActionData.prototype, {
     totalDmg: {
         get() {
-            return this.dmg + this.echoDmg;
+            return this.dmg + this.echoDmg + this.skillDmg + this.ougiDmg;
         }
     },
     totalHits: {
@@ -301,6 +302,7 @@ function BattleCharData(id, name = "") {
         echoDmg: 0,
         echoHits: 0,
         skillDmg: 0,
+        ougiDmg: 0,
         crits: 0,
         da: 0,
         ta: 0,
@@ -333,13 +335,16 @@ const battleCharStatsShared = {
         return safeDivide(this.ougis, this.instance.activeTurns) * 100;
     },
     get totalDmg() {
-        return this.dmg + this.echoDmg + this.skillDmg;
+        return this.dmg + this.echoDmg + this.skillDmg + this.ougiDmg;
     },
     get skillDmgPerc() {
         return safeDivide(this.skillDmg, this.totalDmg) * 100;
     },
     get echoDmgPerc() {
         return safeDivide(this.echoDmg, this.totalDmg) * 100;
+    },
+    get ougiDmgPerc() {
+        return safeDivide(this.ougiDmg, this.totalDmg) * 100;
     },
     get totalHits() {
         return this.hits + this.echoHits;
@@ -376,22 +381,27 @@ function battleParseDamage(input, actionData, type) {
                     actionData.dmgTaken[char] += parseInt(entry.value);
                     break;
                 case BATTLE_ACTION_TYPES.dmgDealt:
-                    if (entry.concurrent_attack_count > 0) {
-                        actionData.echoDmg += parseInt(entry.value);
+                    let dmg = parseInt(entry.value);
+                    if (entry.concurrent_attack_count > 0) { //echo
+                        actionData.echoDmg += dmg;
                         actionData.echoHits++;
                     }
                     else {
-                        if (actionData.action == BATTLE_ACTIONS.skill) {
-                            actionData.skillDmg += parseInt(entry.value);
-                        }
-                        else {
-                            actionData.dmg += parseInt(entry.value);
+                        switch(actionData.action) {
+                            case BATTLE_ACTIONS.skill:
+                                actionData.skillDmg += dmg;
+                                break;
+                            case BATTLE_ACTIONS.ougi:
+                                actionData.ougiDmg += dmg;
+                                break;
+                            default: //normal atk
+                                actionData.dmg += dmg;
                         }
                         actionData.hits++;
                     }
 
-                    if (entry.critical) {
-                        actionData.critDmg += parseInt(entry.value);
+                    if (entry.critical) { //crit is not a "source" of dmg
+                        actionData.critDmg += dmg;
                         actionData.crits++;
                     }
                     if (entry.miss > 0) {
@@ -661,6 +671,7 @@ function updateBattleStats(actionData) {
         charStats.echoHits += actionData.echoHits;
         charStats.echoDmg += actionData.echoDmg;
         charStats.skillDmg += actionData.skillDmg;
+        charStats.ougiDmg += actionData.ougiDmg;
         charStats.crits += actionData.crits;
         if (actionData.action == BATTLE_ACTIONS.ougi) {
             charStats.ougis++;

src/pages/devtools/ui.html
@@ -287,8 +287,8 @@
                                 </div>
                                 <div class="battle-stats-container">
                                     <div class="battle-stats-group chara">
-                                        <div class="battle-stats" data-stat="totalDmg">Dmg: <span>0</span></div>
-                                        <div class="battle-stats" data-stat="avgDmg">Avg Dmg: <span>0</span></div>
+                                        <div class="battle-stats" data-stat="totalDmg">Total dmg: <span>0</span></div>
+                                        <div class="battle-stats" data-stat="avgDmg">Avg dmg: <span>0</span></div>
                                     </div>
                                     <div class="battle-stats-group chara">
                                         <div class="battle-stats" data-stat="daRate">DA: <span data-deci="true">0</span>%</div>
@@ -296,18 +296,26 @@
                                     </div>
                                     <div class="battle-stats" data-stat="critRate">Crit: <span data-deci="true">0</span>%</div>
                                     <div class="battle-stats" data-stat="ougiRate">Ougi: <span data-deci="true">0</span>%</div>
-                                    <div class="sep"></div>
-                                    <div class="battle-stats-group chara">
-                                        <div class="battle-stats" data-stat="skillDmg">Skill dmg: <span>0</span></div>
-                                        <div class="battle-stats" data-stat="echoDmg">Echo dmg: <span>0</span></div>
+                                    
+                                    <div class="sep" data-event="collapse" data-in="panel" data-id="cStatDmgB"></div>
+                                    <div class="battle-stats-container">
+                                        <div class="battle-stats-group chara">
+                                            <div class="battle-stats" data-stat="skillDmg">Skill dmg: <span>0</span></div>
+                                            <div class="battle-stats" data-stat="echoDmg">Echo dmg: <span>0</span></div>
+                                        </div>
+                                        <div class="battle-stats-group chara">
+                                            <div class="battle-stats" data-stat="skillDmgPerc">Skill %: <span>0</span></div>
+                                            <div class="battle-stats" data-stat="echoDmgPerc">Echo %: <span>0</span></div>
+                                        </div>
+                                        <div class="battle-stats" data-stat="ougiDmg">Ougi dmg: <span>0</span></div>
+                                        <div class="battle-stats" data-stat="ougiDmgPerc">Ougi %: <span>0</span>%</div>
                                     </div>
-                                    <div class="battle-stats-group chara">
-                                        <div class="battle-stats" data-stat="skillDmgPerc">Skill %: <span>0</span></div>
-                                        <div class="battle-stats" data-stat="echoDmgPerc">Echo %: <span>0</span></div>
+                                    
+                                    <div class="sep" data-event="collapse" data-in="panel" data-id="cStatHeal"></div>
+                                    <div class="battle-stats-container">
+                                        <div class="battle-stats" data-stat="selfHealing">Heal (self): <span>0</span></div>
+                                        <div class="battle-stats" data-stat="teamHealing">Heal (team): <span>0</span></div>
                                     </div>
-                                    <div class="sep"></div>
-                                    <div class="battle-stats" data-stat="selfHealing">Heal (self): <span>0</span></div>
-                                    <div class="battle-stats" data-stat="teamHealing">Heal (team): <span>0</span></div>
                                 </div>
                             </div>
                         </template>

src/pages/devtools/ui.js
@@ -237,7 +237,21 @@ function evhGlobalClick (e) {
     if (e.target.dataset.event) { //dataset is always accesible
         switch (e.target.dataset.event) {
             case "collapse":
-                e.target.nextElementSibling.classList.toggle('hidden');
+                //collapse all in <parent> for <id>
+                if (e.target.dataset.in && e.path) { //chrome only but so is gbf (technically)
+                    let cId = e.target.dataset.id;
+                    for (let parent of e.path) {
+                        if (parent.classList && parent.classList.contains(e.target.dataset.in)) {
+                            let cList = parent.querySelectorAll(`[data-id='${cId}']`);
+                            for (let node of cList) {
+                                node.nextElementSibling.classList.toggle('hidden');
+                            }
+                        }
+                    }
+                }
+                else {
+                    e.target.nextElementSibling.classList.toggle('hidden');
+                }
                 break;
             case "filter":
                 let activeFilters = [];

src/pages/stylesheets/ui.css
@@ -388,6 +388,9 @@ button {
     grid-template-columns: 1fr 1fr;
     grid-template-areas: "left right";
 }
+.battle-stats-container .battle-stats-container { /*Nested*/
+    grid-area: auto / left /auto / right;
+}
 .battle-stats-container .sep {
     grid-area: auto / left / auto / right;
 }