noccu/veera

commit 9cb60853f4c4fefc59723d25d7d8b92df6c512c9

Supply updates. Merge supply events into one.
Effect is all supply types are now sent to the UI and thus visible.
Refining on #4.

- setTreasure and setConsumables -> updSupplies
- Add a "get all" function
- Move initializing supplies UI out of the load function, into main.

src/background/supplies.js
@@ -300,6 +300,9 @@ window.Supplies = {
         }
         return ret;
     },
+    getAll() {
+        return this.getType(Object.keys(this.index));
+    },
     /**Set the full treasure index, for use with game's supplies page.*/
     setTreasure: function (json) {
         //TODO: For weapon planner we need items we may not have yet. So gotta init a basic array from a datastore.
@@ -312,7 +315,7 @@ window.Supplies = {
 
         this.set(upd);
         this.save();
-        updateUI("setTreasure", upd); //TODO: merge the event to "update supplies" as it now sends through the type explicitly
+        updateUI("updSupplies", upd); //TODO: merge the event to "update supplies" as it now sends through the type explicitly
     },
     /**Set the full consumables index, for use with game's supplies page.*/
     setConsumables: function (json) {
@@ -365,15 +368,14 @@ window.Supplies = {
             }
             else { //Add new
                 item.count = item.delta;
-                this.set(item); //_upd not called with object = no this
+                this.set(item);
             }
         }
         for (let item of updArr) {
             _upd.call(this, item);
         }
 
-        updateUI("setTreasure", updArr.filter(e => e.type == SUPPLYTYPE.treasure));
-        updateUI("setConsumables", updArr.filter(e => SUPPLYTYPE.Consumables.includes(e.type)));
+        updateUI("updSupplies", updArr);
         this.save();
     },
     save: function() {
@@ -387,8 +389,7 @@ window.Supplies = {
                 Supplies.index = data.sup_idx || {};
 
                 devlog("Supply index loaded.");
-                updateUI("setTreasure", Supplies.getType(SUPPLYTYPE.treasure));
-                updateUI("setConsumables", Supplies.getType(SUPPLYTYPE.Consumables));
+//                updateUI("updSupplies", Supplies.getAll());
                 r();
             }
             Storage.get(["sup_idx"], _load);

src/pages/devtools/main.js
@@ -105,21 +105,20 @@ function updateStatus (data) {
 function updateSupplies (idx) {
     if (idx) {
         var list = document.getElementById("supplies-list");
-        var temp;
+        var newItems;
 
         for (let item of idx) {
             let entry = document.getElementById(`${item.type}_${item.id}`);
-            if (entry) {
+            if (entry) { //update
                 entry.getElementsByClassName("collection-data")[0].textContent = item.count;
             }
-            else {
-                if (!temp) { temp = document.createElement("template"); }
-                var li = createSupplyItem(item);
-                temp.content.appendChild(li);
+            else { //add new
+                if (!newItems) { newItems = document.createElement("template"); }
+                newItems.content.appendChild(createSupplyItem(item));
             }
         }
-        if (temp) {
-            list.appendChild(temp.content);
+        if (newItems) {
+            list.appendChild(newItems.content);
         }
     }
 }
@@ -130,19 +129,17 @@ function createSupplyItem (data) {
         return;
     }
     var t = document.getElementById("template-supply-item");
-/*    t.content.querySelector("li").title = name;
-    t.content.querySelector("img").src = thumb;
-    t.content.querySelector("div").textContent = num;*/
     let item = t.content.firstElementChild;
     item.id = `${data.type}_${data.id}`;
-    let loc = data.location,
-        name = data.name;
-    item.title = loc ? `${name}\nGet from: ${loc}`: name;
+    item.title = data.location ? `${data.name}\nGet from: ${data.location}`: data.name;
     item.getElementsByClassName("collection-icon")[0].src = data.path;
     item.getElementsByClassName("collection-data")[0].textContent = data.count;
     item.dataset.type = data.typeName;
     if (data.metaType) {
         item.dataset.metaType = data.metaType;
+    }
+    else {
+        delete item.dataset.metaType;
     }
 //    item.classList.add(data.typeName.replace(" ",""));
 

src/pages/devtools/ui.html
@@ -134,7 +134,7 @@
                                 <ul id="supplies-filter">
                                     <li class="active" data-event="filter" data-value="ALL">All</li>
 <!--                                    Todo: fill list programatically-->
-                                    <li data-event="filter" data-value="Consumables" data-meta="true">Consumabes</li>
+                                    <li data-event="filter" data-value="Consumables" data-meta="true">Consumables</li>
                                     <li data-event="filter" data-value="Treasure">Treasure</li>
                                     <li data-event="filter" data-value="Rings">Rings</li>
                                     <li data-event="filter" data-value="Gauph Keys">Keys</li>

src/pages/devtools/message.js
@@ -47,10 +47,8 @@ window.BackgroundPage = {
             case "updStatus":
                 updateStatus(msg.data);
                 break;
-            case "setTreasure":
-            case "setConsumables":
+            case "updSupplies":
                 updateSupplies(msg.data);
-//                updateConsumables(msg.data);
                 syncPlanner(msg.data, "treasure");
                 break;
             case "newPlannerSeriesOptions":

src/background/background.js
@@ -25,6 +25,7 @@ function MainInit() {
             updateUI("init_plannerSeriesList", Planner.listSeries());
             updateUI("init_unfEdition", State.unfEdition);
             updateUI("init_raidList", Raids.getList());
+            updateUI("updSupplies", Supplies.getAll());
         })
         .then(checkReset);
 }

commit 8cef1dce8d38ceba4504b553c722fe57f75f0891

Supply updates.
- Read tickets from supplies page and add a filter for them.
- Fix consumables page not updating the UI.
  - Wow that's a regression from 9cb60853f4c4fefc
- Update code to allow item types lacking icon size in URL.
- Revert to type numbers for supply filtering.

src/background/message.js
@@ -60,6 +60,9 @@ function hear (msg) {
                 case url.ismatch("item/recovery_and_evolution_list")://Consumables list
                     Supplies.setConsumables(msg.data.json);
                     break;
+                case url.ismatch("item/gacha_ticket_and_others_list_by_filter_mode"):
+                    Supplies.setTickets(msg.data.json);
+                    break;
                 case url.ismatch("resultmulti/data")://Raid loot screen
                 case url.ismatch("result/data"): //Quest loot screen
                 case url.ismatch("arcarum/open_chest"):

src/background/supplies.js
@@ -52,7 +52,8 @@ const ITEM_KIND = {//jshint ignore:line
     "8": {
         "name": "Draw Ticket",
         "class": "Ticket",
-        "path": "item/ticket"
+        "path": "item/ticket",
+        noSize: true
     },
     "9": {
         "name": "Crystal",
@@ -225,16 +226,13 @@ const ITEM_SPECIAL_ID = {
 };
 
 function SupplyItem (type = SUPPLYTYPE.treasure, id, count = 0, name = "Unknown") {
-    if (!Number.isInteger(count)) {
-        throw new TypeError("[SupplyItem] Invalid count.");
-    }
     this.type = parseInt(type);
     this.id = parseInt(id);
-    if (Number.isNaN(this.type) || Number.isNaN(this.id)) {
-        throw new TypeError("[SupplyItem] Type or id do not resolve to number.");
+    this.count = parseInt(count);
+    if (Number.isNaN(this.type) || Number.isNaN(this.id) || Number.isNaN(this.count)) {
+        throw new TypeError("[SupplyItem] Type, id, or count does not resolve to number.");
     }
     this.name = name;
-    this.count = count;
     this.typeName = ITEM_KIND[type] ? ITEM_KIND[type].name : "Unknown";
 
     for (let t in SUPPLYTYPE) {
@@ -253,7 +251,7 @@ function SupplyItem (type = SUPPLYTYPE.treasure, id, count = 0, name = "Unknown"
             this.path = "";
         }
         else {
-            this.path = `${GAME_URL.baseGame}${GAME_URL.assets_light}${ITEM_KIND[type].path}/${GAME_URL.size.small}${fname}.jpg`;
+            this.path = `${GAME_URL.baseGame}${GAME_URL.assets_light}${ITEM_KIND[type].path}/${ITEM_KIND[type].noSize ? "" : GAME_URL.size.small}${fname}.jpg`;
         }
     }
     if (type == SUPPLYTYPE.treasure && TREASURE_SOURCES[id]) {
@@ -345,7 +343,7 @@ window.Supplies = {
 
         this.set(upd);
         this.save();
-        updateUI("updSupplies", upd); //TODO: merge the event to "update supplies" as it now sends through the type explicitly
+        updateUI("updSupplies", upd);
     },
     /**Set the full consumables index, for use with game's supplies page.*/
     setConsumables: function (json) {
@@ -380,11 +378,24 @@ window.Supplies = {
 
         this.set(upd);
         this.save();
-        updateUI("setConsumables", upd);
+        updateUI("updSupplies", upd);
     },
     /** Updates supply item data, adding if new. Note: only used for incremental updates, use set() otherwise.
     @arg {{type, id, delta:number, name:string}[]} updArr
     */
+    setTickets (json) {
+        if (json) {
+            let upd = [];
+            for (let arr of json) {
+                for (let item of arr) {
+                    upd.push( new SupplyItem(SUPPLYTYPE.drawTickets, item.item_id, item.number, item.name) );
+                }
+            }
+            this.set(upd);
+            this.save();
+            updateUI("updSupplies", upd);
+        }
+    },
     update: function(updArr) {
         function _upd(item) {
             //            let itemData = ITEM_KIND[type];
@@ -624,7 +635,7 @@ function consumePendingRaidsTreasure (data) {
 }
 
 function weaponUncapStart(data) {
-    var update = { id: data.url.match(/materials\/(\d+)\?/)[1],
+    var update = { id: data.url.match(/materials\/(\d+)/)[1],
                    items: [] };
 
 //    update.id = data.url.match(/materials\/(\d+)\?/)[1];

src/pages/devtools/main.js
@@ -130,6 +130,7 @@ function updateSupplies (idx) {
         for (let item of idx) {
             let entry = document.getElementById(`${item.type}_${item.id}`);
             if (entry) { //update
+                entry.title = item.name;
                 entry.getElementsByClassName("collection-data")[0].textContent = item.count;
             }
             else { //add new
@@ -154,16 +155,14 @@ function createSupplyItem (data, idPrefix) {
     item.title = data.location ? `${data.name}\nGet from: ${data.location}`: data.name;
     item.getElementsByClassName("collection-icon")[0].src = data.path;
     item.getElementsByClassName("collection-data")[0].textContent = data.count;
-    item.dataset.type = data.typeName;
+    
+    let newEle = document.importNode(t.content, true);
+    newEle.firstElementChild.dataset.type = data.type;
     if (data.metaType) {
-        item.dataset.metaType = data.metaType;
+        newEle.firstElementChild.dataset.metaType = data.metaType;
     }
-    else {
-        delete item.dataset.metaType;
-    }
-//    item.classList.add(data.typeName.replace(" ",""));
-
-    return document.importNode(t.content, true);
+    
+    return newEle;
 }
 
 //Planner functions

src/pages/devtools/ui.html
@@ -140,10 +140,11 @@
 <!--                                    Todo: fill list programatically-->
                                     <li data-event="filter" data-value="Consumables" data-meta="true">Consumables</li>
                                     <li data-event="filter" data-value="Currencies" data-meta="true">Currencies</li>
-                                    <li data-event="filter" data-value="Treasure">Treasure</li>
-                                    <li data-event="filter" data-value="Rings">Rings</li>
-                                    <li data-event="filter" data-value="Gauph Keys">Keys</li>
-                                    <li data-event="filter" data-value="Arcarum Items">Arcarum</li>
+                                    <li data-event="filter" data-value="10">Treasure</li>
+                                    <li data-event="filter" data-value="73">Rings</li>
+                                    <li data-event="filter" data-value="67">Keys</li>
+                                    <li data-event="filter" data-value="65">Arcarum</li>
+                                    <li data-event="filter" data-value="8">Tickets</li>
                                 </ul>
                                 <input id="supplies-search" class="panel-search"/>
         <!--                        <div class="sep"></div>-->