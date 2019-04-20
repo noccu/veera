noccu/veera

commit 2d06010530c1cda70dd0a6f6187951a5267a7e42

Minor Profile refactor.
- Track arca.
- Show currently tracked data in UI top display.
- Consolidate profile parsing.
- Quiet errors on home page redirects causing lack of data.

src/background/profile.js
@@ -1,58 +1,4 @@
 window.Profile = {
-    get currencies() { //TODO: maybe better as object return
-        return Supplies.get([
-            {type: SUPPLYTYPE.rupie, id: 0},
-            {type: 19, id: 0},
-            {type: SUPPLYTYPE.crystals, id: 0}
-        ]);
-    },
-    pendants: {
-        count: {
-            //Here to show the structure cleanly, doesn't actually need to be changed on updates.
-            renown: {
-                total: {current: 0, max: 0},
-                daily: {current: 0, max: 0},
-                weekly: {current: 0, max: 0},
-                sr: {current: 0, max: 0},
-                r: {current: 0, max: 0},
-            },
-            prestige: {
-                total: {current: 0, max: 0},
-                weekly: {current: 0, max: 0},
-                crew: {current: 0, max: 0}
-            }
-        },
-
-        update: function (updArr) { //[ { pendantType, limitType, delta }]
-            for (let item of updArr) {
-                this.count[item.pendantType][item.limitType].current += item.delta;
-//                this.count[item.pendantType][item.limitType].max += item.max;
-            }
-            updateUI("updPendants", this.count);
-        },
-        set: function (json) {
-            if (!json.option) {State.deverror("Error finding pendant info."); return;}
-            var pendants = json.option.mbp_limit_info;
-            var renown = pendants['92001'];
-            var prestige = pendants['92002'];
-            this.count = {
-                renown: {
-                    total: {current: parseInt(renown.article.number), max: parseInt(renown.article.limit)},
-                    weekly: {current: parseInt(renown.limit_info['10100'].data.weekly.get_number), max: parseInt(renown.limit_info['10100'].data.weekly.get_limit)},
-                    daily: {current: parseInt(renown.limit_info['10100'].data.daily.get_number), max: parseInt(renown.limit_info['10100'].data.daily.get_limit)},
-                    sr: {current: parseInt(renown.limit_info['20200'].data.weekly.get_number), max: parseInt(renown.limit_info['20200'].data.weekly.get_limit)},
-                    r: {current: parseInt(renown.limit_info['20100'].data.weekly.get_number), max: parseInt(renown.limit_info['20100'].data.weekly.get_limit)}
-                    },
-
-                prestige: {
-                    total: {current: parseInt(prestige.article.number), max: parseInt(prestige.article.limit)},
-                    weekly: {current: parseInt(prestige.limit_info['10100'].data.weekly.get_number), max: parseInt(prestige.limit_info['10100'].data.weekly.get_limit)},
-                    crew: {current: parseInt(prestige.limit_info['20300'].data.weekly.get_number), max:  parseInt(prestige.limit_info['20300'].data.weekly.get_limit)}
-                }
-            };
-            updateUI("updPendants", this.count);
-        }
-    },
     status: {
         ap: {current: 0, max: 0},
         bp: {current: 0, max: 0},
@@ -60,16 +6,120 @@ window.Profile = {
         level: 0,
         levelPerc: 0,
         jobLevel: 0,
-        exp: 0,
+        exp: 0
+    },
+    get currencies() {
+        return {
+            rupie: Supplies.get(SUPPLYTYPE.rupie, 0),
+            cp: Supplies.get(19, 0),
+            crystals: Supplies.get(SUPPLYTYPE.crystals, 0)
+        }
+    },
+    pendants: {
+        renown: {
+            total: {current: 0, max: 0},
+            daily: {current: 0, max: 0},
+            weekly: {current: 0, max: 0},
+            sr: {current: 0, max: 0},
+            r: {current: 0, max: 0},
+        },
+        prestige: {
+            total: {current: 0, max: 0},
+            weekly: {current: 0, max: 0},
+            crew: {current: 0, max: 0}
+        }
+    },
+    arcarum: {
+        tickets: {},
+        points: {}
+    },
 
-        set: function (json) {
-            this.ap = {current: json.now_action_point, max: parseInt(json.max_action_point)};
-            this.bp = {current: json.now_battle_point, max: json.max_battle_point};
+    setPendants (pendants) {
+        var renown = pendants['92001'];
+        var prestige = pendants['92002'];
+        this.pendants = {
+            renown: {
+                total: {current: parseInt(renown.article.number), max: parseInt(renown.article.limit)},
+                weekly: {current: parseInt(renown.limit_info['10100'].data.weekly.get_number), max: parseInt(renown.limit_info['10100'].data.weekly.get_limit)},
+                daily: {current: parseInt(renown.limit_info['10100'].data.daily.get_number), max: parseInt(renown.limit_info['10100'].data.daily.get_limit)},
+                sr: {current: parseInt(renown.limit_info['20200'].data.weekly.get_number), max: parseInt(renown.limit_info['20200'].data.weekly.get_limit)},
+                r: {current: parseInt(renown.limit_info['20100'].data.weekly.get_number), max: parseInt(renown.limit_info['20100'].data.weekly.get_limit)}
+            },
 
-            this.level = parseInt(json.level);
-            this.levelPerc = parseInt(json.levelGauge.slice(0,-1));
+            prestige: {
+                total: {current: parseInt(prestige.article.number), max: parseInt(prestige.article.limit)},
+                weekly: {current: parseInt(prestige.limit_info['10100'].data.weekly.get_number), max: parseInt(prestige.limit_info['10100'].data.weekly.get_limit)},
+                crew: {current: parseInt(prestige.limit_info['20300'].data.weekly.get_number), max:  parseInt(prestige.limit_info['20300'].data.weekly.get_limit)}
+            }
+        };
+        updateUI("updPendants", this.pendants);
+    },
+    updatePendants (updArr) { //[ { pendantType, limitType, delta }]
+        for (let item of updArr) {
+            this.pendants[item.pendantType][item.limitType].current += item.delta;
+//                this.count[item.pendantType][item.limitType].max += item.max;
+        }
+        updateUI("updPendants", this.pendants);
+    },
+    setCurrencies (dom) {
+        let info = dom.querySelector(".prt-info-possessed");
+        Supplies.set(new SupplyItem(SUPPLYTYPE.rupie, 0, parseInt(info.children[0].textContent), "Rupie"));
+        Supplies.set(new SupplyItem(19, 0, parseInt(info.children[1].textContent), "CP"));
+        Supplies.set(new SupplyItem(SUPPLYTYPE.crystals, 0, parseInt(info.children[2].textContent), "Crystals"));
 
-            updateUI("updStatus", {ap: this.ap.current, apMax: this.ap.max, bp: this.bp.current, lvl: this.level, lvlP: this.levelPerc});
+        Supplies.save();
+        updateUI("updCurrencies", this.currencies);
+    },
+    setArca (dom) {
+        //TODO: check if arca gives us this in easier fashion
+        let info = dom.querySelector("#arcarum-status");
+        let text = info.children[0].textContent.split("/");
+        this.arcarum.tickets.current = text[0];
+        this.arcarum.tickets.max = text[1];
+        text = info.children[1].textContent.split("/");
+        this.arcarum.points.current = text[0];
+        this.arcarum.points.max = text[1];
+
+        updateUI("updArca", this.arcarum);
+    },
+    setStatus (status) {
+        this.status.ap = {
+            current: status.now_action_point,
+            max: parseInt(status.max_action_point)
+        };
+        this.status.bp = {
+            current: status.now_battle_point,
+            max: status.max_battle_point
+        };
+
+        this.status.level = parseInt(status.level);
+        this.status.levelPerc = parseInt(status.levelGauge.slice(0, -1));
+
+        updateUI("updStatus", this.status);
+    },
+    update (json) {
+        //Sometimes returns no info, like on gacha banners or other redirects.
+        if (json.data) { //Currencies & arca
+            let dom = parseDom(json.data);
+            this.setCurrencies(dom);
+            this.setArca(dom);
+        }
+
+        let status = json.status
+        if (json.option) {
+            if (!status && json.option.mydata_assets && json.option.mydata_assets.mydata) {
+                status = json.option.mydata_assets.mydata.status;
+            }
+
+            //Pendants
+            if (json.option.mbp_limit_info) {
+                this.setPendants(json.option.mbp_limit_info);
+            }
+        }
+
+        //Status
+        if (status) {
+            this.setStatus(status);
         }
     }
 };
@@ -81,31 +131,21 @@ function getPendantsRaid (json) {
         switch (json.mbp_info.item_id) {
             case "92001": //renown
                 //It only gives a total number, taking into account daily and weekly, but we still have to add them.
-                Profile.pendants.update([{pendantType: "renown", limitType: "daily", delta: added['10100'].add_point},
-                                         {pendantType: "renown", limitType: "weekly", delta: added['10100'].add_point},
-                                         {pendantType: "renown", limitType: "sr", delta: added['20200'].add_point},
-                                         {pendantType: "renown", limitType: "r", delta: added['20100'].add_point},
-                                         {pendantType: "renown", limitType: "total", delta: parseInt(json.mbp_info.article_remain['92001'].number) + json.mbp_info.number} //Using given number to deal with multibox
-                                         ]);
+                Profile.pendants.update([
+                    {pendantType: "renown", limitType: "daily", delta: added['10100'].add_point},
+                    {pendantType: "renown", limitType: "weekly", delta: added['10100'].add_point},
+                    {pendantType: "renown", limitType: "sr", delta: added['20200'].add_point},
+                    {pendantType: "renown", limitType: "r", delta: added['20100'].add_point},
+                    {pendantType: "renown", limitType: "total", delta: parseInt(json.mbp_info.article_remain['92001'].number) + json.mbp_info.number} //Using given number to deal with multibox
+                ]);
                 break;
             case "92002": //prestige
-                Profile.pendants.update([ {pendantType: "prestige", limitType: "weekly", delta: added['10100'].add_point},
-                                          {pendantType: "prestige", limitType: "crew", delta: added['20300'].add_point},
-                                          {pendantType: "prestige", limitType: "total", delta: parseInt(json.mbp_info.article_remain['92002'].number) + json.mbp_info.number} //Using given number to deal with multibox
-                                        ]);
+                Profile.pendants.update([
+                    {pendantType: "prestige", limitType: "weekly", delta: added['10100'].add_point},
+                    {pendantType: "prestige", limitType: "crew", delta: added['20300'].add_point},
+                    {pendantType: "prestige", limitType: "total", delta: parseInt(json.mbp_info.article_remain['92002'].number) + json.mbp_info.number} //Using given number to deal with multibox
+                ]);
                 break;
         }
     }
-}
-
-function setCurrencies(json) {
-    let dom = parseDom(json.data);
-    let c = dom.querySelector(".prt-info-possessed");
-
-    Supplies.set(new SupplyItem(SUPPLYTYPE.rupie, 0, parseInt(c.children[0].textContent), "Rupie"));
-    Supplies.set(new SupplyItem(19, 0, parseInt(c.children[1].textContent), "CP"));
-    Supplies.set(new SupplyItem(SUPPLYTYPE.crystals, 0, parseInt(c.children[2].textContent), "Crystals"));
-
-    Supplies.save();
-    updateUI("updSupplies", Profile.currencies);
 }

src/background/message.js
@@ -50,15 +50,9 @@ function hear (msg) {
             // var req = u.slice(30, u.lastIndexOf("/"));
             switch (true) {
                 case url.ismatch("user/content/index"): //Homepage
-                    Profile.pendants.set(msg.data.json);
-                    Profile.status.set(msg.data.json.option.mydata_assets.mydata.status);
-                    setCurrencies(msg.data.json);
-                    break;
+                case url.ismatch("profile/content/index"): //Profile
                 case url.ismatch("user/status"):
-                    Profile.status.set(msg.data.json.status);
-                    break;
-                case url.ismatch("profile/content/index"):
-                    Profile.pendants.set(msg.data.json);
+                    Profile.update(msg.data.json);
                     break;
                 case url.ismatch("item/article_list")://Treasure list
                     Supplies.setTreasure(msg.data.json);

src/pages/devtools/main.js
@@ -84,29 +84,42 @@ function updatePendants (data) {
         }
     ]);
 }
-
 function updateStatus (data) {
-    UI.setValue([{
-        id: "ap-current",
-        value: data.ap
-    },{
-        id: "bp-current",
-        value: data.bp
-    },{
-        id: "ap-max",
-        value: data.apMax
-    },{
-        id: "rank",
-        value: data.lvl
-    }/*,{
-        id: "",
-        value: data.lvlP
-    }*/
-    //TODO: level bar
-    ]);
+    UI.setValue([
+        {id: "ap-current", value: data.ap.current},
+        {id: "bp-current", value: data.bp.current},
+        {id: "ap-max", value: data.ap.max},
+        {id: "rank", value: data.level}
+        /*,{
+            id: "",
+            value: data.lvlP
+            }*/ 
+        //TODO: level bar
+    ], true);
 
     var d = document.getElementById("ap-bp-display");
     d.classList.toggle("highlight", data.ap > data.apMax || data.bp > 10);
+}
+function updateArca(data) {
+    UI.setValue([
+        {id: "num-arc-tck", value: data.tickets.current},
+        {id: "max-arc-tck", value: data.tickets.max},
+        {id: "num-arc-pts", value: data.points.current},
+        {id: "max-arc-pts", value: data.points.max}
+    ], true);
+}
+function updateCurrencies(data) {
+    UI.setValue([
+        {id: "num-rupies", value: data.rupie.count},
+        {id: "num-cp", value: data.cp.count},
+        {id: "num-crystal", value: data.crystals.count}
+    ], true);
+
+    updateSupplies([
+        data.rupie,
+        data.cp,
+        data.crystals
+    ]);
 }
 
 function updateSupplies (idx) {

src/pages/devtools/message.js
@@ -29,11 +29,17 @@ window.BackgroundPage = {
             case "init":
                 initialize(msg.data);
                 break;
+            case "updStatus":
+                updateStatus(msg.data);
+                break;
             case "updPendants":
                 updatePendants(msg.data);
                 break;
-            case "updStatus":
-                updateStatus(msg.data);
+            case "updCurrencies":
+                updateCurrencies(msg.data);
+                break;
+            case "updArca":
+                updateArca(msg.data);
                 break;
             case "updSupplies":
                 updateSupplies(msg.data);

src/pages/devtools/ui.html
@@ -21,40 +21,40 @@
         <div id="top-display">
             <div id="profile">
                 <div id="ap-bp-display" class="display">
-                    <div id="ap-display">AP: <span id="ap-current" class="current-value">100</span>/<span id="ap-max" class="max-value">106</span></div>
-                    <div id="bp-display">BP: <span id="bp-current" class="current-value">10</span>/<span id="bp-max" class="max-value">10</span></div>
+                    <div id="ap-display">AP: <span id="ap-current" class="current-value">0</span>/<span id="ap-max" class="max-value">0</span></div>
+                    <div id="bp-display">BP: <span id="bp-current" class="current-value">0</span>/<span id="bp-max" class="max-value">0</span></div>
                 </div>
                 <div id="status-display" class="display">
                     <div class="value-display">
-                        Rank: <span id="rank">100</span>
+                        Rank: <span id="rank">0</span>
                     </div>
                     <div class="value-display">
-                        RP: <span id="rank-pts">100</span>
+                        RP: <span id="rank-pts">0</span>
                     </div>
                     <div class="value-display">
-                        Class lvl: <span id="class-lvl">20</span>
+                        Class lvl: <span id="class-lvl">0</span>
                     </div>
                 </div>
                 <div id="currency-display" class="display">
                     <div class="div-hor">
-                        <div class="value-display">Crystals: <span id="num-crystal">60625</span></div>
-                        <div class="value-display">Rupies: <span id="num-rupies">86525</span></div>
-                        <div class="value-display">Chips: <span id="num-chips">6,099,625</span></div>
+                        <div class="value-display">Crystals: <span id="num-crystal">0</span></div>
+                        <div class="value-display">Rupies: <span id="num-rupies">0</span></div>
+                        <div class="value-display">Chips: <span id="num-chips">0</span></div>
                     </div>
                     <div class="div-hor">
-                        <div class="value-display">CP: <span id="num-cp">5000</span></div>
-                        <div class="value-display">EMP: <span id="num-emp">500</span></div>
-                        <div class="value-display">JD: <span id="num-jd">6625</span></div>
+                        <div class="value-display">CP: <span id="num-cp">0</span></div>
+                        <div class="value-display">EMP: <span id="num-emp">0</span></div>
+                        <div class="value-display">JD: <span id="num-jd">0</span></div>
                     </div>
                     <div class="spacer"></div>
                     <div id="pendants-display">
                         <div class="div-hor">
-                            <div class="value-display">Renown: <div><span id="display-renown-total" class="current-value">12320</span>/<span id="display-renown-max" class="max-value">20000</span></div></div>
-                            <div class="value-display">Prestige: <div><span id="display-prestige-total" class="current-value">1355</span>/<span id="display-prestige-max" class="max-value">5000</span></div></div>
+                            <div class="value-display">Renown: <span><span id="display-renown-total" class="current-value">0</span>/<span id="display-renown-max" class="max-value">0</span></span></div>
+                            <div class="value-display">Prestige: <span><span id="display-prestige-total" class="current-value">0</span>/<span id="display-prestige-max" class="max-value">0</span></span></div>
                         </div>
                         <div class="div-hor">
-                            <div class="value-display">Arcarum tck: <div><span id="num-arc-tck" class="current-value">9</span>/<span id= "max-arc-tck" class="max-value">9</span></div></div>
-                            <div class="value-display">Arcarum pts: <span id="num-arc-pts">18650</span></div>
+                            <div class="value-display">Arcarum tck: <span><span id="num-arc-tck" class="current-value">0</span>/<span id= "max-arc-tck" class="max-value">0</span></span></div>
+                            <div class="value-display">Arcarum pts: <span><span id="num-arc-pts" class="current-value">0</span>/<span id= "max-arc-pts" class="max-value">0</span></span></div>
                         </div>
                     </div>
                 </div>
@@ -171,7 +171,7 @@
                     </div>
                     <div class="spacer"></div>
                     <div id="planner-panel" class="panel">
-                        <span class="panel-header" data-event="collapse">Step 1</span>
+                        <span class="panel-header" data-event="collapse">Total items</span>
                         <div class="panel-content">
                             <div class="spacer"></div>
                             <ul id="planner-list" class="supply-list">

commit 6eb9d168bfdbc91ca9f6fd351e09c01bd1721b88

Fix pendants not updating from raids.
Regression from 2d06010530c1cd.

src/background/profile.js
@@ -177,7 +177,7 @@ function getPendantsRaid (json) {
         switch (json.mbp_info.item_id) {
             case "92001": //renown
                 //It only gives a total number, taking into account daily and weekly, but we still have to add them.
-                Profile.pendants.update([
+                Profile.updatePendants([
                     {pendantType: "renown", limitType: "daily", delta: added['10100'].add_point},
                     {pendantType: "renown", limitType: "weekly", delta: added['10100'].add_point},
                     {pendantType: "renown", limitType: "sr", delta: added['20200'].add_point},
@@ -186,7 +186,7 @@ function getPendantsRaid (json) {
                 ]);
                 break;
             case "92002": //prestige
-                Profile.pendants.update([
+                Profile.updatePendants([
                     {pendantType: "prestige", limitType: "weekly", delta: added['10100'].add_point},
                     {pendantType: "prestige", limitType: "crew", delta: added['20300'].add_point},
                     {pendantType: "prestige", limitType: "total", delta: parseInt(json.mbp_info.article_remain['92002'].number) + json.mbp_info.number} //Using given number to deal with multibox