0fafd85bd9ee08176079505d9b4852193da9c6ba
noccu <37862870+noccu@users.noreply.github.com>
2019-05-02 23:26:52 +0200

    Network update.
    Some refactoring, but the main point is we now see HTML requests.
    Also lessens the amount of errors logged.

--- src/background/message.js
+++ src/background/message.js
@@ -43,141 +43,148 @@ window.DevTools = {
 String.prototype.ismatch = function(s){ return this.indexOf(s) != -1;};
 
 function hear (msg) {
-    devlog("[background] Heard:", msg, msg.action == "request" ? msg.data.url.match(/jp\/([^?]+)/)[1] : "");
+    //Convert to util object. Makes it easier to deal with and is in the end likely faster since we can check much smaller strings.
+    let path = "";
+    if (msg.action == "request") {
+        msg.data.url = new URL(msg.data.url);
+        path = msg.data.url.pathname;
+    }
+    devlog("[background] Heard:", msg, path);
     switch (msg.action) {
         case "request":
-            var url = msg.data.url;
-            // var req = u.slice(30, u.lastIndexOf("/"));
-            switch (true) {
-                case url.ismatch("user/content/index"): //Homepage
-                case url.ismatch("profile/content/index"): //Profile
-                case url.ismatch("user/status"):
-                    Profile.update(msg.data.json);
-                    break;
-                case url.ismatch("casino/content/index"):
-                case url.ismatch("casino/content/list"):
-                    Profile.setCasino(msg.data.json);
-                    break;
-                case url.ismatch("item/article_list")://Treasure list
-                    Supplies.setTreasure(msg.data.json);
-                    break;
-                case url.ismatch("item/recovery_and_evolution_list")://Consumables list
-                    Supplies.setConsumables(msg.data.json);
-                    break;
-                case url.ismatch("item/gacha_ticket_and_others_list_by_filter_mode"):
-                    Supplies.setTickets(msg.data.json);
-                    break;
-                case url.ismatch("resultmulti/data")://Raid loot screen
-                case url.ismatch("result/data"): //Quest loot screen
-                case url.ismatch("arcarum/open_chest"):
-                    gotQuestLoot(msg.data.json);
-                    getPendantsRaid(msg.data.json);
-                    checkNextQuest(msg.data.json);
-                    break;
-                case url.ismatch("weapon/evolution_materials"):
-                    weaponUncapStart(msg.data);//TBH Just xhr the supplies lmao
-                    break;
-                case url.ismatch("npc/evolution_materials"):
-                    npcUncapStart(msg.data);//TBH Just xhr the supplies lmao
-                    break;
-                case url.ismatch("evolution_weapon/item_evolution"):
-                case url.ismatch("evolution_npc/item_evolution"):
-                    uncapEnd(msg.data.json);
-                    break;
-                case /\/teamraid\d+\//.test(url):
-                    DevTools.send("updUnfEdition", url);
-                    break;
-                case url.ismatch("/bookmaker/content/top"): //bookmaker is only in unf r-right?
-                    DevTools.send("updUnfAreas", msg.data.json);
-                    break;
-                case url.ismatch("rest/raid/ability_result.json"):
-                case url.ismatch("rest/multiraid/ability_result.json"):
-                    battleUseAbility(msg.data.json);
-                    break;
-                case url.ismatch("rest/raid/normal_attack_result.json"):
-                case url.ismatch("rest/multiraid/normal_attack_result.json"):
-                    battleAttack(msg.data.json);
-                    break;
-                case url.ismatch("rest/raid/summon_result"):
-                case url.ismatch("rest/multiraid/summon_result"):
-                    battleUseSummon(msg.data.json);
-                    break;
-                case url.ismatch("rest/raid/start"):
-                case url.ismatch("rest/multiraid/start"):
-                    Battle.reset(msg.data.json);
-                    break;
-                case url.ismatch("shop_exchange/purchase"):
-                    purchaseItem(msg.data.json);
-                    //TODO: May need to ignore the next item/article_list request cause some items go to crate, causing the data in the response to not be updated...
-                    break;
-                case url.ismatch("summon/decompose_multi"):
-                case url.ismatch("weapon/decompose_multi"):
-                    reduce(msg.data.json);
-                    break;
-                    //quest/user_action_point  ==> before usage
-                    /*
-                    json:
-                        action_point: 141
-                        action_point_limit: 999
-                        elixir_half_recover_value: 56
-                        elixir_recover_value: 113
-                        max_action_point: "113"
-                    */
-                    //quest/quest_data ==> info
-                    /*
-                    json:
-                        action_point: "40"
-                        chapter_name: "Belial Impossible"
-                    */
-                case url.ismatch("quest/treasure_raid"):
-                case /treasureraid\d+\/top\/content\/newindex/.test(url):
-                    storePendingRaidsTreasure(msg.data.json);
-                    break;
-                case url.ismatch("quest/create_quest"):
-                    if (msg.data.json.result == "ok") {
-                        consumePendingRaidsTreasure(msg.data);
-                        Raids.update({
-                            action: "hosted",
-                            id: msg.data.postData.quest_id
-                        });
-                    }
-                    break;
-                case url.ismatch("disabled_job/"):
-                    storePendingJobUnlock(msg.data.json);
-                    break;
-                case url.ismatch("party/release_job"):
-                    consumePendingJobUnlock(msg.data);
-                    break;
-                case url.ismatch("archaic/job/replica_exchange/"):
-                case url.ismatch("archaic/job/original_exchange/"):
-                case url.ismatch("archaic/job/rebuilt_exchange/"):
-                    storePendingForgeCCW(msg.data.json);
-                    break;
-                case url.ismatch("archaic/job/replica_exchange_result"):
-                case url.ismatch("archaic/job/original_exchange_result"):
-                case url.ismatch("archaic/job/rebuilt_exchange_result"):
-                    consumePendingForgeCCW(msg.data.postData);
-                    break;
-                //There is a confirm for _all which triggers if not checking end of path, hence split.
-                case url.ismatch("present/receive?"):
-                case url.ismatch("present/receive_all?"):
-                case url.ismatch("present/term_receive?"):
-                case url.ismatch("present/term_receive_all?"):
-                    //is  normally followed by article list but that only shows treasure. There's more to pick up than that...
-                    cratePickup(msg.data.json);
-                    break;
-                case url.ismatch("rest/title/par_claim"): //trophy pickup
-                    trophyPickup(msg.data.json);
-                    break;
-                case url.ismatch("advent/top/content/newindex"):
-                    setRotbPendants(msg.data.json);
-                    break;
-                case url.ismatch("guild_main/content/index"):
-                    updStrikeTime(msg.data.json);
-                    break;
-                case url.ismatch("rest/guild/main/guild_info"):
-                    updateGuildInfo(msg.data.json);
-                    break;
+            //JSON
+            if (msg.data.hasOwnProperty("json")) {
+                switch (true) {
+                    case path.ismatch("user/content/index"): //Homepage
+                    case path.ismatch("profile/content/index"): //Profile
+                    case path.ismatch("user/status"):
+                        Profile.update(msg.data.json);
+                        break;
+                    case path.ismatch("casino/content/index"):
+                    case path.ismatch("casino/content/list"):
+                        Profile.setCasino(msg.data.json);
+                        break;
+                    case path.ismatch("item/article_list")://Treasure list
+                        Supplies.setTreasure(msg.data.json);
+                        break;
+                    case path.ismatch("item/recovery_and_evolution_list")://Consumables list
+                        Supplies.setConsumables(msg.data.json);
+                        break;
+                    case path.ismatch("item/gacha_ticket_and_others_list_by_filter_mode"):
+                        Supplies.setTickets(msg.data.json);
+                        break;
+                    case path.ismatch("resultmulti/data")://Raid loot screen
+                    case path.ismatch("result/data"): //Quest loot screen
+                    case path.ismatch("arcarum/open_chest"):
+                        gotQuestLoot(msg.data.json);
+                        getPendantsRaid(msg.data.json);
+                        checkNextQuest(msg.data.json);
+                        break;
+                    case path.ismatch("weapon/evolution_materials"):
+                        weaponUncapStart(msg.data);//TBH Just xhr the supplies lmao
+                        break;
+                    case path.ismatch("npc/evolution_materials"):
+                        npcUncapStart(msg.data);//TBH Just xhr the supplies lmao
+                        break;
+                    case path.ismatch("evolution_weapon/item_evolution"):
+                    case path.ismatch("evolution_npc/item_evolution"):
+                        uncapEnd(msg.data.json);
+                        break;
+                    case /teamraid\d+\//.test(path):
+                        DevTools.send("updUnfEdition", msg.data.url.href);
+                        break;
+                    case path.ismatch("/bookmaker/content/top"): //bookmaker is only in unf r-right?
+                        DevTools.send("updUnfAreas", msg.data.json);
+                        break;
+                    case path.ismatch("rest/raid/ability_result.json"):
+                    case path.ismatch("rest/multiraid/ability_result.json"):
+                        battleUseAbility(msg.data.json);
+                        break;
+                    case path.ismatch("rest/raid/normal_attack_result.json"):
+                    case path.ismatch("rest/multiraid/normal_attack_result.json"):
+                        battleAttack(msg.data.json);
+                        break;
+                    case path.ismatch("rest/raid/summon_result"):
+                    case path.ismatch("rest/multiraid/summon_result"):
+                        battleUseSummon(msg.data.json);
+                        break;
+                    case path.ismatch("rest/raid/start"):
+                    case path.ismatch("rest/multiraid/start"):
+                        Battle.reset(msg.data.json);
+                        break;
+                    case path.ismatch("shop_exchange/purchase"):
+                        purchaseItem(msg.data.json);
+                        //TODO: May need to ignore the next item/article_list request cause some items go to crate, causing the data in the response to not be updated...
+                        break;
+                    case path.ismatch("summon/decompose_multi"):
+                    case path.ismatch("weapon/decompose_multi"):
+                        reduce(msg.data.json);
+                        break;
+                        //quest/user_action_point  ==> before usage
+                        /*
+                        json:
+                            action_point: 141
+                            action_point_limit: 999
+                            elixir_half_recover_value: 56
+                            elixir_recover_value: 113
+                            max_action_point: "113"
+                        */
+                        //quest/quest_data ==> info
+                        /*
+                        json:
+                            action_point: "40"
+                            chapter_name: "Belial Impossible"
+                        */
+                    case path.ismatch("quest/treasure_raid"):
+                    case /treasureraid\d+\/top\/content\/newindex/.test(path):
+                        storePendingRaidsTreasure(msg.data.json);
+                        break;
+                    case path.ismatch("quest/create_quest"):
+                        if (msg.data.json.result == "ok") {
+                            consumePendingRaidsTreasure(msg.data);
+                            Raids.update({
+                                action: "hosted",
+                                id: msg.data.postData.quest_id
+                            });
+                        }
+                        break;
+                    case path.ismatch("disabled_job/"):
+                        storePendingJobUnlock(msg.data.json);
+                        break;
+                    case path.ismatch("party/release_job"):
+                        consumePendingJobUnlock(msg.data);
+                        break;
+                    case path.ismatch("archaic/job/replica_exchange/"):
+                    case path.ismatch("archaic/job/original_exchange/"):
+                    case path.ismatch("archaic/job/rebuilt_exchange/"):
+                        storePendingForgeCCW(msg.data.json);
+                        break;
+                    case path.ismatch("archaic/job/replica_exchange_result"):
+                    case path.ismatch("archaic/job/original_exchange_result"):
+                    case path.ismatch("archaic/job/rebuilt_exchange_result"):
+                        consumePendingForgeCCW(msg.data.postData);
+                        break;
+                    //There is a confirm for _all which triggers if not checking end of path, hence split.
+                    case path.ismatch("present/receive?"):
+                    case path.ismatch("present/receive_all?"):
+                    case path.ismatch("present/term_receive?"):
+                    case path.ismatch("present/term_receive_all?"):
+                        //is  normally followed by article list but that only shows treasure. There's more to pick up than that...
+                        cratePickup(msg.data.json);
+                        break;
+                    case path.ismatch("rest/title/par_claim"): //trophy pickup
+                        trophyPickup(msg.data.json);
+                        break;
+                    case path.ismatch("advent/top/content/newindex"):
+                        setRotbPendants(msg.data.json);
+                        break;
+                    case path.ismatch("guild_main/content/index"):
+                        updStrikeTime(msg.data.json);
+                        break;
+                    case path.ismatch("rest/guild/main/guild_info"):
+                        updateGuildInfo(msg.data.json);
+                        break;
+                }
             }
             break;
         case "plannerSeriesChange":
--- src/pages/devtools/network.js
+++ src/pages/devtools/network.js
@@ -10,23 +10,57 @@ window.Network = {
         this.logging = false;
         console.log("Request listener removed.");
     },
-    logRequest: function(entry) {
-        //Ignore anything that isn't JSON from gbf site.
+    logRequest: async function(entry) {
         if (entry.request.url.indexOf("game.granbluefantasy.jp") != -1) {
-            if (entry.response.content.mimeType == "application/json") {
-                entry.getContent(data => {
-                    if (DEBUG) { console.debug(entry.request.url.slice(30), JSON.parse(data)); }
-                    BackgroundPage.send("request", {url: entry.request.url,
-                                                    json: JSON.parse(data),
-                                                    postData: entry.request.postData ? JSON.parse(entry.request.postData.text) : null
-                                                   });
-                });
+            let data = { url: entry.request.url };
+            let success = false,
+                content;
+            
+            try {
+                data.postData = entry.request.postData ? JSON.parse(entry.request.postData.text) : null;
+                switch (entry.response.content.mimeType) {
+                    case "application/json":
+                        content = await Network.getData(entry);
+                        if (content) {
+                            data.json = JSON.parse(content);
+                            success = true;
+                        }
+                        break;
+                    case "text/html":
+                        content = await Network.getData(entry);
+                        if (content) {
+                            data.body = content;
+                            success = true;
+                        }
+                        
+                        if (entry.request.url.indexOf("maintenance") != -1) { //TODO: move to bg
+                            startMaintTimer(data.body);
+                            return;
+                        }
+                        break;
+                }
             }
-            else if (entry.request.url.indexOf("maintenance") != -1 && entry.response.content.mimeType == "text/html") {
-                entry.getContent(data => startMaintTimer(data));
-                if (DEBUG) { console.debug(entry); }
+            catch (e) {
+                success = false;
+                console.error(e);
+            }
+            
+            if (success) {
+                BackgroundPage.send("request", data);
             }
         }
+    },
+    getData(harEntry) {
+        return new Promise(
+            (r, x) => {
+                try {
+                    harEntry.getContent((content, encoding) => r(encoding == "base64" ? atob(content) : content));
+                }
+                catch (e) {
+                    x(e);
+                }
+            }
+        );
     },
     toggle: function(){
         if (Network.logging) {
