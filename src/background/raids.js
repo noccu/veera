noccu/veera

commit 7488016e4af51e94bc85583ee2c0b2ab724f66f4

Raid changes.
- Do not host raids without the required AP or rank.
- Colour code raids that cost more AP than you have.
- Hide raids you can't host based on rank.
- Settings to control all this behaviour. (ap one useful for magfes)
- Slight refactor of UI raid code.

src/background/raids.js
@@ -45,11 +45,15 @@ function RaidEntry(id, trackingObj) {
             last: null
         };
         this.active = true;
-
     }
+    this.updHostState();
 }
-RaidEntry.prototype.canHost = function() {
-    return this.hosts.today < this.data.dailyHosts;
+
+RaidEntry.prototype.updHostState = function() {
+    this.haveHosts = this.hosts.today < this.data.dailyHosts;
+    this.haveRank = Profile.status.level ? this.data.minHostRank <= Profile.status.level : true;
+    this.haveAp = Profile.status.ap.current ? this.data.apCost <= Profile.status.ap.current : true;
+    this.canHost = this.haveAp && this.haveHosts && this.haveRank;
 };
 
 window.Raids = {
@@ -144,6 +148,7 @@ window.Raids = {
                 break;
         }
         this.set(raidEntry);
+        raidEntry.updHostState();
 
         this.save();
         updateUI("updRaid", raidEntry);
@@ -171,7 +176,7 @@ window.Raids = {
             }
         }
         let url = raid.data.urls[hostMatId || this.NO_HOST_MAT];
-        if (url && sufficientMats) {
+        if (url && sufficientMats && (raid.haveAp || !State.settings.blockHostByAP) && (raid.haveRank || !State.settings.hideRaidsByRank)) {
             State.game.navigateTo(url);
             if (hostMatId) {
                 // eslint-disable-next-line no-undef
@@ -185,7 +190,7 @@ window.Raids = {
         }
         else {
             updateUI("updRaid", raid); // update the hostmat display
-            deverror(`Can't start raid ${id}. Sufficient mats: ${sufficientMats}, url: ${url}`);
+            deverror(`Can't start raid ${raid.data.name} (${id}). Sufficient mats: ${sufficientMats}, Sufficient AP: ${raid.haveAp}, Sufficient Rank: ${raid.haveRank}, url: ${url}`);
         }
     },
     checkUsedMats(used, costs) {

src/background/profile.js
@@ -108,6 +108,8 @@ window.Profile = {
     },
     setStatus(status) {
         let currentAp = status.now_action_point || status.action_point;
+        let lastRank = this.status.level,
+            lastAp = this.status.ap.current;
         if (currentAp) {
             this.status.ap = {
                 current: currentAp,
@@ -127,6 +129,9 @@ window.Profile = {
         }
 
         updateUI("updStatus", this.status);
+        if (this.status.ap.current != lastAp || this.status.level != lastRank) {
+            updateUI("updRaid", Raids.getList());
+        }
     },
     setCasino(json) {
         if (json.data) {

src/background/state.js
@@ -20,7 +20,9 @@ window.State = {
         notifyNmTrigger: true,
 
         focusGameOnAction: true,
-        focusOnlyMinimized: false
+        focusOnlyMinimized: false,
+        hideRaidsByRank: true,
+        blockHostByAP: true // TODO: Add a warning
     },
 
     theme: {

src/pages/devtools/main.js
@@ -265,46 +265,9 @@ function createRaid(raidEntry) {
             addMaterial(mat, matContainer);
         }
     }
-    updateRaidTrackingDisplay(newRaid.firstElementChild);
+    UI.raids.updDisplay(newRaid.firstElementChild);
 
     return newRaid;
-}
-function updateRaidTrackingDisplay(raidEle) {
-    let raidEntry = raidEle.entryObj;
-
-    let hostsLeft = raidEntry.data.dailyHosts - raidEntry.hosts.today;
-    raidEle.querySelector(".raid-hosts .current-value").textContent = hostsLeft;
-    let outOfMats = false;
-
-    function updMats(list) {
-        for (let mat of list) {
-            if (Array.isArray(mat)) {
-                updMats(mat);
-            }
-            else {
-                raidEle.querySelector(`[data-mat-id='${mat.id}'] .current-value`).textContent = mat.supplyData.count;
-                outOfMats = outOfMats || mat.supplyData.count < mat.num;
-            }
-        }
-    }
-    if (raidEntry.data.matCost) {
-        updMats(raidEntry.data.matCost);
-    }
-
-    // CSS
-    if (raidEntry.active) {
-        raidEle.classList.remove("hidden");
-    }
-    else {
-        raidEle.classList.add("hidden");
-    }
-
-    if (hostsLeft == 0 || outOfMats) {
-        raidEle.classList.add("fade");
-    }
-    else {
-        raidEle.classList.remove("fade");
-    }
 }
 function populateRaids(raids) {
     let list = document.getElementById("raids-list");

src/pages/devtools/ui.html
@@ -475,6 +475,9 @@
                 <div class="panel">
                     <span class="panel-header" data-event="collapse">Visilibity & usability</span>
                     <div class="panel-content">
+                        <input data-global-setting="true" type="checkbox" name="hideRaidsByRank"><label>Hide raids over your rank</label>
+                        <input data-global-setting="true" type="checkbox" name="blockHostByAP"><label>Don't host raid when AP insufficient</label>
+                        <div class="spacer"></div>
                         <input data-global-setting="true" type="checkbox" name="focusGameOnAction"><label>Focus game when interacting</label>
                         <input data-global-setting="true" type="checkbox" name="focusOnlyMinimized"><label>Only focus when minimized</label>
                     </div>

src/pages/devtools/ui.js
@@ -233,19 +233,68 @@ window.UI = {
                 raidEntry: raid.entryObj
             });
         },
-        update(raidEntry) {
-            if (Array.isArray(raidEntry)) {
-                for (let re of raidEntry) {
-                    this.update(re);
+        update(raidEntry, loop) {
+            if (raidEntry) {
+                if (Array.isArray(raidEntry)) {
+                    for (let re of raidEntry) {
+                        this.update(re, true);
+                    }
+                }
+                else {
+                    let el = document.getElementById(raidEntry.data.id);
+                    el.entryObj = raidEntry;
+                    this.updDisplay(el);
+                    if (loop) { return }
                 }
             }
             else {
-                let el = document.getElementById(raidEntry.data.id);
-                el.entryObj = raidEntry;
-                updateRaidTrackingDisplay(el);
+                this.list.forEach(raid => this.updDisplay(raid));
             }
 
             document.getElementById("raids-filter").click();
+        },
+        updDisplay(raidEle) {
+            let raidEntry = raidEle.entryObj;
+
+            let hostsLeft = raidEntry.data.dailyHosts - raidEntry.hosts.today;
+            raidEle.querySelector(".raid-hosts .current-value").textContent = hostsLeft;
+            let outOfMats = false;
+
+            function updMats(list) {
+                for (let mat of list) {
+                    if (Array.isArray(mat)) {
+                        updMats(mat);
+                    }
+                    else {
+                        raidEle.querySelector(`[data-mat-id='${mat.id}'] .current-value`).textContent = mat.supplyData.count;
+                        outOfMats = outOfMats || mat.supplyData.count < mat.num;
+                    }
+                }
+            }
+            if (raidEntry.data.matCost) {
+                updMats(raidEntry.data.matCost);
+            }
+
+            // CSS
+            if (raidEntry.active && (raidEntry.haveRank || !SETTINGS.hideRaidsByRank) ) {
+                raidEle.classList.remove("hidden");
+            }
+            else {
+                raidEle.classList.add("hidden");
+            }
+
+            if (!raidEntry.haveHosts || outOfMats) {
+                raidEle.classList.add("fade");
+            }
+            else {
+                raidEle.classList.remove("fade");
+                if (raidEntry.haveAp) {
+                    raidEle.getElementsByClassName("raid-cost")[0].classList.remove("warn");
+                }
+                else {
+                    raidEle.getElementsByClassName("raid-cost")[0].classList.add("warn");
+                }
+            }
         }
     }
 };
@@ -371,6 +420,7 @@ function evhSuppliesFilter(e) {
 function evhRaidsFilter(e) {
     let filters = e.detail;
     function filter(raid) {
+        // [do check, value of check]
         let check = {
             elementName: [false, false],
             tierName: [false, false]
@@ -395,6 +445,7 @@ function evhRaidsFilter(e) {
                 check[f.group][1] |= raid.entryObj.data[f.group] == f.value;
             }
         });
+        if (SETTINGS.hideRaidsByRank) { ret = ret && raid.entryObj.haveRank }
         if (ret) {
             for (let c in check) {
                 if (check[c][0]) {

commit a57962922e6681e99a8caff7cea010760de7e755

Fix raid visibility toggling.
Regression from 7488016e4af51e94bc85583ee2c0b2ab724f66f4.

src/background/raids.js
@@ -46,14 +46,14 @@ function RaidEntry(id, trackingObj) {
         };
         this.active = true;
     }
-    this.updHostState();
+    RaidEntry.updHostState(this);
 }
 
-RaidEntry.prototype.updHostState = function() {
-    this.haveHosts = this.hosts.today < this.data.dailyHosts;
-    this.haveRank = Profile.status.level ? this.data.minHostRank <= Profile.status.level : true;
-    this.haveAp = Profile.status.ap.current ? this.data.apCost <= Profile.status.ap.current : true;
-    this.canHost = this.haveAp && this.haveHosts && this.haveRank;
+RaidEntry.updHostState = function(obj) {
+    obj.haveHosts = obj.hosts.today < obj.data.dailyHosts;
+    obj.haveRank = Profile.status.level ? obj.data.minHostRank <= Profile.status.level : true;
+    obj.haveAp = Profile.status.ap.current ? obj.data.apCost <= Profile.status.ap.current : true;
+    obj.canHost = obj.haveAp && obj.haveHosts && obj.haveRank;
 };
 
 window.Raids = {
@@ -129,6 +129,7 @@ window.Raids = {
     // Updates the tracking object.
     update: function({action, id, matId, raidEntry}) {
         // pendingHost is used here as setLastHost is called afterwards, and copies from it.
+        // raidEntry is just a way to skip the lookup when we already have the data, but it loses class when transfered from UI. Maybe it's more potential harm than good long term...
         if (!raidEntry) {
             if (!id) {
                 deverror(`Invalid data format, can't update raid ${id}.`);
@@ -156,7 +157,7 @@ window.Raids = {
                 break;
         }
         this.set(raidEntry);
-        raidEntry.updHostState();
+        RaidEntry.updHostState(raidEntry);
 
         this.save();
         updateUI("updRaid", raidEntry);